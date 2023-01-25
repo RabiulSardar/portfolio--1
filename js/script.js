4a761f4ad18f1d89bedd55c944392fdb33665a1a
Rabiul <robiulsardar247@gmail.com>
2023-01-26 03:15:51 +0600

    kjhh

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-// //
